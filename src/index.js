cc30e9de804900384d66e05712a03c8c8b9d03d5
Wyatt Sweet <wyattsweet@Wyatts-MacBook-Pro.local>
2017-05-10 21:29:37 -0700

    toggling a todo reducer created

--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ import './index.css';
 const todo = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-    return [
-      ...state,
-      {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    ]
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
     default: 
       return state;
   }
