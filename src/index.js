4ccea678939410e4bfca5fc81cec51cede19d1ad
wyattsweet <wyattsweet@gmail.com>
2017-05-14 11:49:33 -0700

    moved redux reducers into their own file

--- src/index.js
+++ src/index.js
@@ -4,56 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const { combineReducers } = Redux;
-
-// Reducer composition abstracts away reducer logic into multiple reducers then lets reducers call each other
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return {
-        ...state,
-        completed: !state.completed
-      }
-  }
-}
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default: 
-      return state;
-  }
-}
-
-const visibilityFilter = (state='SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
-};
-
-const todoApp = combineReducers({
-  todos, 
-  visibilityFilter
-})
+import todoApp from './redux/todoApp'
 
 const { createStore } = Redux;
 const store = createStore(todoApp);
