977b12670457b3fa1f0bf1657a21051ca53d1285
Wyatt Sweet <wyattsweet@Wyatts-MacBook-Pro.local>
2017-05-16 22:36:43 -0700

    Extracting Presentation Components, Todo and Todolist

--- src/index.js
+++ src/index.js
@@ -98,6 +98,38 @@ const getVisibleTodos = (
   }
 }
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: 
+        completed ? 
+          'line-through' : 
+          'none'
+    }}>
+    {text}
+  </li>
+)
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo => 
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
 class TodoApp extends Component {
   render() {
     const { todos, visibilityFilter } = this.props;
@@ -120,23 +152,14 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                })
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through':'none'
-              }}
-            >
-              {todo.text}  
-            </li>
-          )}
-        </ul>
+        <TodoList 
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })}
+        />
         <p>
           Show:
           {' '}
